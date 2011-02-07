6e26712b3b9196a562329646e30d9153e28ecd74
Patrick Roberts <patrick@silentbluesystems.com>
2011-02-07 15:18:21 -0800

    New Refactor

--- regexMaster.js
+++ regexMaster.js
@@ -6,58 +6,13 @@ var master = regex.master = function()
  {
     this.matches = [];
 }
-//Creates a new single option regex client and sets the callback.  Returns self
-master.prototype.new = function(regex, cb, cat)
- {
-    m = new regexMatch.client();
-    if (cb)
-    {
-        m.
-        if (regex).call(cb);
-    }
-    else
-    {
-        m.
-        if (regex).call("home")
-    }
-    this.matches.push(m);
-    return this;
 
-}
 //Creates a regex and returns it to allow for easy chaining
 master.prototype.construct = function(regex)
  {
     m = new regexMatch.client();
 	if(regex)
-    m.if(regex);
-    this.matches.push(m);
-    return m;
+	return m.if(regex);
+	else
+	return m;
 }
-//Run match against every regex object in memory
-//TODO: Add a limiter to only match certain objects
-master.prototype.match = function(str,cat)
- {
-    var res = false;
-    _.each(this.matches,
-    function(m) {
-        if (res)
-        {}
-        else
-        {
-            res = m.match(str,cat)
-        }
-    })
-    return res;
-}
-//Return all Regex objects
-//TODO: Function
-master.prototype.all = function()
- {
-	return this.matches
-    }
-//Remove a regex object that matches the str
-//TODO: Function
-master.prototype.remove = function(str)
- {
-
-    }
--- test.js
+++ test.js
@@ -1,2 +1,4 @@
 //Note: There are no real tests in here yet.
-
+var master=require('./regexMaster');
+var regex=new master.master();
+console.log(regex.construct().if("hello there <%name%>").if(" this is a <%test%> of the emergency node system").or(" this is NOT a <%nottest%> of the emergency node system").if("more regex <%now%>").or("less regex <%now%>").createRegex().createString());
